243e9746d4f842ac23def430cbf119eecfc80a04
Carly Morris <thabai@hotmail.com>
2021-08-03 14:38:51 +0100

    findAllMovies created

--- src/movies/movies.routes.js
+++ src/movies/movies.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const movieRouter = Router();
-const { addMovie, findMovie, deleteMovie, updateMovie } = require("./movies.controllers");
+const { addMovie, findMovie, findAllMovies, deleteMovie, updateMovie } = require("./movies.controllers");
 
 movieRouter.post("/movies", addMovie);
 movieRouter.get("/movies/:title", findMovie);
+movieRouter.get("/moviesAll", findAllMovies);
 movieRouter.delete("/movies/:title", deleteMovie);
 movieRouter.put("/movies", updateMovie);
 
